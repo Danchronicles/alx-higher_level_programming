a02b1cd34bda1811430e659eb4503c191ba18b76
Danchronicles <dankipchumba669@gmail.com>
2025-10-21 07:39:03 +0300

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """
+    size: size length of the square
+    """
+    
+    # Handle TypeError
+    if not isinstance(size,):
+        raise TypeError("size must be an interger")
+    
+    # Handle ValueError
+    if size <= 0 and float:
+        raise ValueError("size must be >= 0")
+        raise TypeError("size must be an interger")
+    
+
+        
+     
